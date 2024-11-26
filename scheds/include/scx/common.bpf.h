be2d676d129a9d99df9aba0e4db23e5092262616
Emil Tsalapatis <etsal@meta.com>
2024-11-26 14:29:19 -0800

    inline cast_mask() to prevent collisions between separate compilation units

--- scheds/include/scx/common.bpf.h
+++ scheds/include/scx/common.bpf.h
@@ -341,7 +341,7 @@ u32 bpf_cpumask_weight(const struct cpumask *cpumask) __ksym;
 /*
  * Access a cpumask in read-only mode (typically to check bits).
  */
-const struct cpumask *cast_mask(struct bpf_cpumask *mask) {
+const inline struct cpumask *cast_mask(struct bpf_cpumask *mask) {
   return (const struct cpumask *)mask;
 }
 
